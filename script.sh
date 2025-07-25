828f20f1cea784ca707ac0a6e9b294f8d2b53cae
ben-aslan <kiziltahaaslan@gmail.com>
2025-07-25 15:05:55 +0330

    feat(installation): add script.sh

---
+++ script.sh
@@ -0,0 +1,716 @@
+#!/usr/bin/env bash
+set -e
+
+APP_NAME="nobetci"
+NODE_NAME="nobetnode"
+CONFIG_DIR="/etc/opt/$APP_NAME"
+DATA_DIR="/var/lib/$APP_NAME"
+NODE_DATA_DIR="/var/lib/$NODE_NAME"
+COMPOSE_FILE="$CONFIG_DIR/docker-compose.yml"
+ENV_FILE="$CONFIG_DIR/.env"
+NOBETNODE_ENV_FILE="$CONFIG_DIR/nobetnode.env"
+PANEL_ADDRESS=""
+PANEL_USER=""
+PANEL_PASSWORD=""
+BOT_TOKEN=""
+CHAT_ID=0
+SECRET_KEY=""
+API_USERNAME=""
+API_PASSWORD=""
+
+FETCH_REPO="muttehitler/nobetci"
+SCRIPT_URL="https://github.com/$FETCH_REPO/raw/master/script.sh"
+
+colorized_echo() {
+    local color=$1
+    local text=$2
+
+    case $color in
+        "red")
+        printf "\e[91m${text}\e[0m\n";;
+        "green")
+        printf "\e[92m${text}\e[0m\n";;
+        "yellow")
+        printf "\e[93m${text}\e[0m\n";;
+        "blue")
+        printf "\e[94m${text}\e[0m\n";;
+        "magenta")
+        printf "\e[95m${text}\e[0m\n";;
+        "cyan")
+        printf "\e[96m${text}\e[0m\n";;
+        *)
+            echo "${text}"
+        ;;
+    esac
+}
+
+check_running_as_root() {
+    if [ "$(id -u)" != "0" ]; then
+        colorized_echo red "This command must be run as root."
+        exit 1
+    fi
+}
+
+detect_os() {
+    # Detect the operating system
+    if [ -f /etc/lsb-release ]; then
+        OS=$(lsb_release -si)
+        elif [ -f /etc/os-release ]; then
+        OS=$(awk -F= '/^NAME/{print $2}' /etc/os-release | tr -d '"')
+        elif [ -f /etc/redhat-release ]; then
+        OS=$(cat /etc/redhat-release | awk '{print $1}')
+        elif [ -f /etc/arch-release ]; then
+        OS="Arch"
+    else
+        colorized_echo red "Unsupported operating system"
+        exit 1
+    fi
+}
+
+detect_and_update_package_manager() {
+    colorized_echo blue "Updating package manager"
+    if [[ "$OS" == "Ubuntu"* ]] || [[ "$OS" == "Debian"* ]]; then
+        PKG_MANAGER="apt-get"
+        $PKG_MANAGER update
+        elif [[ "$OS" == "CentOS"* ]] || [[ "$OS" == "AlmaLinux"* ]]; then
+        PKG_MANAGER="yum"
+        $PKG_MANAGER update -y
+        $PKG_MANAGER install -y epel-release
+        elif [ "$OS" == "Fedora"* ]; then
+        PKG_MANAGER="dnf"
+        $PKG_MANAGER update
+        elif [ "$OS" == "Arch" ]; then
+        PKG_MANAGER="pacman"
+        $PKG_MANAGER -Sy
+    else
+        colorized_echo red "Unsupported operating system"
+        exit 1
+    fi
+}
+
+detect_compose() {
+    # Check if docker compose command exists
+    if docker compose >/dev/null 2>&1; then
+        COMPOSE='docker compose'
+        elif docker-compose >/dev/null 2>&1; then
+        COMPOSE='docker-compose'
+    else
+        colorized_echo red "docker compose not found"
+        exit 1
+    fi
+}
+
+install_package () {
+    if [ -z $PKG_MANAGER ]; then
+        detect_and_update_package_manager
+    fi
+
+    PACKAGE=$1
+    colorized_echo blue "Installing $PACKAGE"
+    if [[ "$OS" == "Ubuntu"* ]] || [[ "$OS" == "Debian"* ]]; then
+        $PKG_MANAGER -y install "$PACKAGE"
+        elif [[ "$OS" == "CentOS"* ]] || [[ "$OS" == "AlmaLinux"* ]]; then
+        $PKG_MANAGER install -y "$PACKAGE"
+        elif [ "$OS" == "Fedora"* ]; then
+        $PKG_MANAGER install -y "$PACKAGE"
+        elif [ "$OS" == "Arch" ]; then
+        $PKG_MANAGER -S --noconfirm "$PACKAGE"
+    else
+        colorized_echo red "Unsupported operating system"
+        exit 1
+    fi
+}
+
+install_docker() {
+    # Install Docker and Docker Compose using the official installation script
+    colorized_echo blue "Installing Docker"
+    curl -fsSL https://get.docker.com | sh
+    colorized_echo green "Docker installed successfully"
+}
+
+install_nobetci_script() {
+    colorized_echo blue "Installing nöbetci script"
+    curl -sSL $SCRIPT_URL | install -m 755 /dev/stdin /usr/local/bin/nobetci
+    colorized_echo green "nöbetci script installed successfully"
+}
+
+install_nobetci() {
+    # Fetch releases
+    FILES_URL_PREFIX="https://raw.githubusercontent.com/muttehitler/nobetci/master"
+	COMPOSE_FILES_URL="https://raw.githubusercontent.com/muttehitler/nobetci/master/deploy"
+ 	database=$1
+  	nightly=$2
+  
+    mkdir -p "$DATA_DIR"
+    mkdir -p "$CONFIG_DIR"
+
+    colorized_echo blue "Fetching compose file"
+    curl -sL "$COMPOSE_FILES_URL/docker-compose-$database.yml" -o "$CONFIG_DIR/docker-compose.yml"
+    colorized_echo green "File saved in $CONFIG_DIR/docker-compose.yml"
+	if [ "$nightly" = true ]; then
+	    colorized_echo red "setting compose tag to nightly."
+	 	sed -ri "s/(ghcr.io\/muttehitler\/nobetci:)latest/\1nightly/g" $CONFIG_DIR/docker-compose.yml
+	fi
+ 
+    colorized_echo blue "Fetching example .env file"
+    curl -sL "$FILES_URL_PREFIX/.env.example" -o "$ENV_FILE"
+    colorized_echo green "File saved in $ENV_FILE"
+
+    colorized_echo green "Nöbetci files downloaded successfully"
+}
+
+install_nobetnode_env() {
+    mkdir -p "$CONFIG_DIR"
+    curl -sL "https://raw.githubusercontent.com/muttehitler/nobetnode/master/.env.example" -o "$NOBETNODE_ENV_FILE"
+
+    if grep -q "^SERVICE_ADDRESS=" "$NOBETNODE_ENV_FILE"; then
+        sed -i.bak "s|^SERVICE_ADDRESS=.*|SERVICE_ADDRESS=127.0.0.1|" "$NOBETNODE_ENV_FILE"
+    else
+        echo "SERVICE_ADDRESS=127.0.0.1" >> "$NOBETNODE_ENV_FILE"
+    fi
+
+    if grep -q "^INSECURE=" "$NOBETNODE_ENV_FILE"; then
+        sed -i.bak "s|^INSECURE=.*|INSECURE=True|" "$NOBETNODE_ENV_FILE"
+    else
+        echo "INSECURE=True" >> "$NOBETNODE_ENV_FILE"
+    fi
+
+    colorized_echo green "Sample environment downloaded for nobetnode"
+}
+
+uninstall_nobetci_script() {
+    if [ -f "/usr/local/bin/nobetci" ]; then
+        colorized_echo yellow "Removing nöbetci script"
+        rm "/usr/local/bin/nobetci"
+    fi
+}
+
+uninstall_nobetci() {
+    if [ -d "$CONFIG_DIR" ]; then
+        colorized_echo yellow "Removing directory: $CONFIG_DIR"
+        rm -r "$CONFIG_DIR"
+    fi
+}
+
+uninstall_nobetci_docker_images() {
+    images=$(docker images | grep nobetci | awk '{print $3}')
+
+    if [ -n "$images" ]; then
+        colorized_echo yellow "Removing Docker images of Nöbetci"
+        for image in $images; do
+            if docker rmi "$image" >/dev/null 2>&1; then
+                colorized_echo yellow "Image $image removed"
+            fi
+        done
+    fi
+}
+
+uninstall_nobetci_data_files() {
+    if [ -d "$DATA_DIR" ]; then
+        colorized_echo yellow "Removing directory: $DATA_DIR"
+        rm -r "$DATA_DIR"
+    fi
+}
+
+uninstall_nobetnode_data_files() {
+    if [ -d "$NODE_DATA_DIR" ]; then
+        colorized_echo yellow "Removing directory: $NODE_DATA_DIR"
+        rm -r "$NODE_DATA_DIR"
+    fi
+}
+
+
+up_nobetci() {
+    $COMPOSE -f $COMPOSE_FILE -p "$APP_NAME" up -d --remove-orphans
+}
+
+down_nobetci() {
+    $COMPOSE -f $COMPOSE_FILE -p "$APP_NAME" down
+}
+
+show_nobetci_logs() {
+    $COMPOSE -f $COMPOSE_FILE -p "$APP_NAME" logs
+}
+
+follow_nobetci_logs() {
+    $COMPOSE -f $COMPOSE_FILE -p "$APP_NAME" logs -f
+}
+
+nobetci_cli() {
+    $COMPOSE -f $COMPOSE_FILE -p "$APP_NAME" exec -e CLI_PROG_NAME="nobetci cli" nobetci /app/cli.py "$@"
+}
+
+
+update_nobetci_script() {
+    colorized_echo blue "Updating nöbetci script"
+    curl -sSL $SCRIPT_URL | install -m 755 /dev/stdin /usr/local/bin/nobetci
+    colorized_echo green "nöbetci script updated successfully"
+}
+
+update_nobetci() {
+    $COMPOSE -f $COMPOSE_FILE -p "$APP_NAME" pull
+}
+
+is_nobetci_installed() {
+    if [ -d $CONFIG_DIR ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+is_nobetci_up() {
+    if [ -z "$($COMPOSE -f $COMPOSE_FILE ps -q -a)" ]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+get_panel_address(){
+    read -p "Enter the panel address: " PANEL_ADDRESS
+
+    if grep -q "^PANEL_ADDRESS=" "$ENV_FILE"; then
+        sed -i.bak "s|^PANEL_ADDRESS=.*|PANEL_ADDRESS=$PANEL_ADDRESS|" "$ENV_FILE"
+    else
+        echo "PANEL_ADDRESS=$PANEL_ADDRESS" >> "$ENV_FILE"
+    fi
+}
+
+get_panel_user(){
+    read -p "Enter the panel user: " PANEL_USER
+
+    if grep -q "^PANEL_USERNAME=" "$ENV_FILE"; then
+        sed -i.bak "s|^PANEL_USERNAME=.*|PANEL_USERNAME=$PANEL_USER|" "$ENV_FILE"
+    else
+        echo "PANEL_USERNAME=$PANEL_USER" >> "$ENV_FILE"
+    fi
+}
+
+get_panel_password(){
+    read -p "Enter the panel password: " PANEL_PASSWORD
+
+    if grep -q "^PANEL_PASSWORD=" "$ENV_FILE"; then
+        sed -i.bak "s|^PANEL_PASSWORD=.*|PANEL_PASSWORD=$PANEL_PASSWORD|" "$ENV_FILE"
+    else
+        echo "PANEL_PASSWORD=$PANEL_PASSWORD" >> "$ENV_FILE"
+    fi
+}
+
+get_bot_token(){
+    read -p "Enter the bot token: " BOT_TOKEN
+
+    if grep -q "^TELEGRAM_API_TOKEN=" "$ENV_FILE"; then
+        sed -i.bak "s|^TELEGRAM_API_TOKEN=.*|TELEGRAM_API_TOKEN=$BOT_TOKEN|" "$ENV_FILE"
+    else
+        echo "TELEGRAM_API_TOKEN=$BOT_TOKEN" >> "$ENV_FILE"
+    fi
+}
+
+get_chat_id(){
+    read -p "Enter the admin chat id: " CHAT_ID
+
+    if grep -q "^TELEGRAM_ADMIN_ID=" "$ENV_FILE"; then
+        sed -i.bak "s|^TELEGRAM_ADMIN_ID=.*|TELEGRAM_ADMIN_ID=$CHAT_ID|" "$ENV_FILE"
+    else
+        echo "TELEGRAM_ADMIN_ID=$CHAT_ID" >> "$ENV_FILE"
+    fi
+}
+
+get_secret_key(){
+    read -p "Enter the api secret key(select a strong secret key): " SECRET_KEY
+
+    if grep -q "^SECRET_KEY=" "$ENV_FILE"; then
+        sed -i.bak "s|^SECRET_KEY=.*|SECRET_KEY=$SECRET_KEY|" "$ENV_FILE"
+    else
+        echo "SECRET_KEY=$SECRET_KEY" >> "$ENV_FILE"
+    fi
+}
+
+get_api_username(){
+    read -p "Enter the api username: " API_USERNAME
+
+    if grep -q "^API_USERNAME=" "$ENV_FILE"; then
+        sed -i.bak "s|^API_USERNAME=.*|API_USERNAME=$API_USERNAME|" "$ENV_FILE"
+    else
+        echo "API_USERNAME=$API_USERNAME" >> "$ENV_FILE"
+    fi
+}
+
+get_api_password(){
+    read -p "Enter the api password: " API_PASSWORD
+
+    if grep -q "^API_PASSWORD=" "$ENV_FILE"; then
+        sed -i.bak "s|^API_PASSWORD=.*|API_PASSWORD=$API_PASSWORD|" "$ENV_FILE"
+    else
+        echo "API_PASSWORD=$API_PASSWORD" >> "$ENV_FILE"
+    fi
+}
+
+install_command() {
+    check_running_as_root
+    # Check if nobetci is already installed
+    if is_nobetci_installed; then
+        colorized_echo red "Nöbetci is already installed at $CONFIG_DIR"
+        read -p "Do you want to override the previous installation? (y/n) "
+        if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+            colorized_echo red "Aborted installation"
+            exit 1
+        fi
+    fi
+    detect_os
+    if ! command -v jq >/dev/null 2>&1; then
+        install_package jq
+    fi
+    if ! command -v curl >/dev/null 2>&1; then
+        install_package curl
+    fi
+    if ! command -v docker >/dev/null 2>&1; then
+        install_docker
+    fi
+	
+    database="sqlite"
+	nightly=false
+ 
+	while [[ "$#" -gt 0 ]]; do
+	    case $1 in
+	        -d|--database)
+		 		database="$2"
+				if [[ ! $database =~ ^(sqlite|mariadb|mysql)$ ]]; then
+				    echo "database could only be sqlite, mysql and mariadb."
+					exit 1
+				fi
+	            shift
+	            ;;
+			-n|--nightly)
+	            nightly=true
+	            ;;
+	        *)
+	            echo "Unknown option: $1"
+	            exit 1
+	            ;;
+	    esac
+	    shift
+	done
+
+    detect_compose
+    install_nobetci_script
+    install_nobetci $database $nightly
+    install_nobetnode_env
+
+    get_panel_address
+    get_panel_user
+    get_panel_password
+    get_bot_token
+    get_chat_id
+    get_secret_key
+    get_api_username
+    get_api_password
+    
+    up_nobetci
+    follow_nobetci_logs
+}
+
+uninstall_command() {
+    check_running_as_root
+    # Check if nobetci is installed
+    if ! is_nobetci_installed; then
+        colorized_echo red "Nöbetci's not installed!"
+        exit 1
+    fi
+
+    read -p "Do you really want to uninstall Nöbetci? (y/n) "
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+        colorized_echo red "Aborted"
+        exit 1
+    fi
+
+    detect_compose
+    if is_nobetci_up; then
+        down_nobetci
+    fi
+    uninstall_nobetci_script
+    uninstall_nobetci
+    uninstall_nobetci_docker_images
+
+    read -p "Do you want to remove nöbetci & nobetnode data files too ($NODE_DATA_DIR, $DATA_DIR)? (y/n) "
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+        colorized_echo green "Nöbetci uninstalled successfully"
+    else
+        uninstall_nobetci_data_files
+        colorized_echo green "Nöbetci uninstalled successfully"
+    fi
+}
+
+up_command() {
+    help() {
+        colorized_echo red "Usage: $0 up [options]"
+        echo ""
+        echo "OPTIONS:"
+        echo "  -h, --help        display this help message"
+        echo "  -n, --no-logs     do not follow logs after starting"
+    }
+
+    local no_logs=false
+    while [[ "$#" -gt 0 ]]; do
+        case "$1" in
+            -n|--no-logs)
+                no_logs=true
+            ;;
+            -h|--help)
+                help
+                exit 0
+            ;;
+            *)
+                echo "Error: Invalid option: $1" >&2
+                help
+                exit 0
+            ;;
+        esac
+        shift
+    done
+
+    # Check if nöbetci is installed
+    if ! is_nobetci_installed; then
+        colorized_echo red "Nöbetci is not installed!"
+        exit 1
+    fi
+
+    detect_compose
+
+    if is_nobetci_up; then
+        colorized_echo red "Nöbetci is already up"
+        exit 1
+    fi
+
+    up_nobetci
+    if [ "$no_logs" = false ]; then
+        follow_nobetci_logs
+    fi
+}
+
+down_command() {
+
+    # Check if nöbetci is installed
+    if ! is_nobetci_installed; then
+        colorized_echo red "Nöbetci's not installed!"
+        exit 1
+    fi
+
+    detect_compose
+
+    if ! is_nobetci_up; then
+        colorized_echo red "Nöbetci's already down"
+        exit 1
+    fi
+
+    down_nobetci
+}
+
+restart_command() {
+    help() {
+        colorized_echo red "Usage: $0 restart [options]"
+        echo
+        echo "OPTIONS:"
+        echo "  -h, --help        display this help message"
+        echo "  -n, --no-logs     do not follow logs after starting"
+    }
+
+    local no_logs=false
+    while [[ "$#" -gt 0 ]]; do
+        case "$1" in
+            -n|--no-logs)
+                no_logs=true
+            ;;
+            -h|--help)
+                help
+                exit 0
+            ;;
+            *)
+                echo "Error: Invalid option: $1" >&2
+                help
+                exit 0
+            ;;
+        esac
+        shift
+    done
+
+    # Check if nöbetci is installed
+    if ! is_nobetci_installed; then
+        colorized_echo red "Nöbetci's not installed!"
+        exit 1
+    fi
+
+    detect_compose
+
+    down_nobetci
+    up_nobetci
+    if [ "$no_logs" = false ]; then
+        follow_nobetci_logs
+    fi
+}
+
+status_command() {
+
+    # Check if nöbetci is installed
+    if ! is_nobetci_installed; then
+        echo -n "Status: "
+        colorized_echo red "Not Installed"
+        exit 1
+    fi
+
+    detect_compose
+
+    if ! is_nobetci_up; then
+        echo -n "Status: "
+        colorized_echo blue "Down"
+        exit 1
+    fi
+
+    echo -n "Status: "
+    colorized_echo green "Up"
+
+    json=$($COMPOSE -f $COMPOSE_FILE ps -a --format=json)
+    services=$(echo "$json" | jq -r 'if type == "array" then .[] else . end | .Service')
+    states=$(echo "$json" | jq -r 'if type == "array" then .[] else . end | .State')
+    # Print out the service names and statuses
+    for i in $(seq 0 $(expr $(echo $services | wc -w) - 1)); do
+        service=$(echo $services | cut -d' ' -f $(expr $i + 1))
+        state=$(echo $states | cut -d' ' -f $(expr $i + 1))
+        echo -n "- $service: "
+        if [ "$state" == "running" ]; then
+            colorized_echo green $state
+        else
+            colorized_echo red $state
+        fi
+    done
+}
+
+logs_command() {
+    help() {
+        colorized_echo red "Usage: nöbetci logs [options]"
+        echo ""
+        echo "OPTIONS:"
+        echo "  -h, --help        display this help message"
+        echo "  -n, --no-follow   do not show follow logs"
+    }
+
+    local no_follow=false
+    while [[ "$#" -gt 0 ]]; do
+        case "$1" in
+            -n|--no-follow)
+                no_follow=true
+            ;;
+            -h|--help)
+                help
+                exit 0
+            ;;
+            *)
+                echo "Error: Invalid option: $1" >&2
+                help
+                exit 0
+            ;;
+        esac
+        shift
+    done
+
+    # Check if nöbetci is installed
+    if ! is_nobetci_installed; then
+        colorized_echo red "Nöbetci is not installed!"
+        exit 1
+    fi
+
+    detect_compose
+
+    if ! is_nobetci_up; then
+        colorized_echo red "Nöbetci is not up."
+        exit 1
+    fi
+
+    if [ "$no_follow" = true ]; then
+        show_nobetci_logs
+    else
+        follow_nobetci_logs
+    fi
+}
+
+cli_command() {
+    # Check if nöbetci is installed
+    if ! is_nobetci_installed; then
+        colorized_echo red "Nöbetci is not installed!"
+        exit 1
+    fi
+
+    detect_compose
+
+    if ! is_nobetci_up; then
+        colorized_echo red "Nöbetci is not up."
+        exit 1
+    fi
+
+    nobetci_cli "$@"
+}
+
+update_command() {
+    check_running_as_root
+    # Check if nöbetci is installed
+    if ! is_nobetci_installed; then
+        colorized_echo red "Nöbetci is not installed!"
+        exit 1
+    fi
+
+    detect_compose
+
+    update_nobetci_script
+    colorized_echo blue "Pulling latest version"
+    update_nobetci
+
+    colorized_echo blue "Restarting Nöbetci's services"
+    down_nobetci
+    up_nobetci
+
+    colorized_echo blue "Nöbetci updated successfully"
+}
+
+
+usage() {
+    colorized_echo red "Usage: $0 [command]"
+    echo
+    echo "Commands:"
+    echo "  up              Start services"
+    echo "  down            Stop services"
+    echo "  restart         Restart services"
+    echo "  status          Show status"
+    echo "  logs            Show logs"
+    echo "  cli             Nöbetci command-line interface"
+    echo "  install         Install Nöbetci"
+    echo "  update          Update latest version"
+    echo "  uninstall       Uninstall Nöbetci"
+    echo "  install-script  Install Nöbetci script"
+    echo
+}
+
+case "$1" in
+    up)
+    shift; up_command "$@";;
+    down)
+    shift; down_command "$@";;
+    restart)
+    shift; restart_command "$@";;
+    status)
+    shift; status_command "$@";;
+    logs)
+    shift; logs_command "$@";;
+    cli)
+    shift; cli_command "$@";;
+    install)
+    shift; install_command "$@";;
+    update)
+    shift; update_command "$@";;
+    uninstall)
+    shift; uninstall_command "$@";;
+    install-script)
+    shift; install_nobetci_script "$@";;
+    *)
+    usage;;
+esac
