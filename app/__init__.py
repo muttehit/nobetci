160ff9c2f52a39f6bc6f3db91ccca1d490e896ba
ben-aslan <kiziltahaaslan@gmail.com>
2025-07-27 00:24:51 +0330

    fix(logging): add format for logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
+import logging
+
+import uvicorn
+from app.config import DEBUG
 from app.db.db_context import DbContext
 from app.db.models import UserLimit
 from app.storage.memory import MemoryStorage
@@ -7,3 +11,12 @@ __version__ = "0.0.1"
 
 storage = MemoryStorage()
 user_limit_db = DbContext(UserLimit)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG if DEBUG else logging.INFO)
+handler = logging.StreamHandler()
+formatter = uvicorn.logging.ColourizedFormatter(
+    "{levelprefix:<8} @{name}: {message}", style="{", use_colors=True
+)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
