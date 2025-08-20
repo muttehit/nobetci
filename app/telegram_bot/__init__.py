1036d39a700f46fcb403b6e582ba7ff7a913845c
ben-aslan <kiziltahaaslan@gmail.com>
2025-08-20 19:04:47 +0330

    feat(telegram): add user command

--- app/telegram_bot/__init__.py
+++ app/telegram_bot/__init__.py
@@ -10,6 +10,12 @@ from telegram.ext import (
     filters,
 )
 from app.config import TELEGRAM_API_TOKEN
+from app import user_limit_db
+
+(
+    GET_USER_NAME,
+    GET_USER_LIMIT
+) = range(2)
 
 
 async def build_telegram_bot():
@@ -17,6 +23,16 @@ async def build_telegram_bot():
         return
 
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(
+        ConversationHandler(
+            entry_points=[CommandHandler("add_user", add_user)],
+            states={
+                GET_USER_NAME: [MessageHandler(filters.TEXT, get_user_name)],
+                GET_USER_LIMIT: [MessageHandler(filters.TEXT, get_user_limit)]
+            },
+            fallbacks=[],
+        )
+    )
 
     while True:
         try:
@@ -32,6 +48,33 @@ async def build_telegram_bot():
 async def start(update: Update, _context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_html(text=START_MESSAGE)
 
+
+async def add_user(update: Update, _context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_html(
+        text="Send Your User Name:"
+    )
+    return GET_USER_NAME
+
+
+async def get_user_name(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    context.user_data["name"] = update.message.text.strip()
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Send Your User Limit: (For example: 1)",
+    )
+    return GET_USER_LIMIT
+
+
+async def get_user_limit(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    context.user_data["limit"] = update.message.text.strip()
+
+    user_limit_db.add({"name": context.user_data["name"], "limit": context.user_data["limit"]})
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"User {context.user_data["name"]} added with limit {context.user_data["limit"]}"
+    )
+    return ConversationHandler.END
 
 START_MESSAGE = """
 <b>Commands List:</b>\n
