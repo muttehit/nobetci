096abe63e4a4c5130a4488ab16fb527bc822cd76
ben-aslan <kiziltahaaslan@gmail.com>
2025-10-23 22:53:33 +0330

    feat(marznode): reconnect marznodes every 2 hours and 27 minutes

--- app/nobetci.py
+++ app/nobetci.py
@@ -9,13 +9,11 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from fastapi.security import OAuth2PasswordBearer
 
-from app.models.panel import Panel
-from app.service.check_service import CheckService
-from app.service.marznode_service import MarzNodeService
+from app.tasks.marznode import start_marznode_tasks
 from app.telegram_bot import build_telegram_bot
 from app.utils.panel import get_marznodes
 
-from . import __version__, user_limit_db, storage
+from . import __version__
 
 from app.config import (DEBUG, DOCS, PANEL_ADDRESS, PANEL_CUSTOM_NODES, PANEL_PASSWORD, PANEL_USERNAME,
                         UVICORN_HOST, UVICORN_PORT, UVICORN_SSL_CERTFILE, UVICORN_SSL_KEYFILE, UVICORN_UDS)
@@ -32,24 +30,9 @@ logger = logging.getLogger(__name__)
 async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     asyncio.create_task(build_telegram_bot())
 
-    paneltype = Panel(
-        username=PANEL_USERNAME,
-        password=PANEL_PASSWORD,
-        domain=PANEL_ADDRESS,
-    )
-
     await nodes_startup()
 
-    node_service = MarzNodeService(CheckService(
-        storage, user_limit_db))
-
-    marznodes = await get_marznodes(paneltype)
-
-    if PANEL_CUSTOM_NODES:
-        marznodes = [m for m in marznodes if m.name in PANEL_CUSTOM_NODES]
-
-    for marznode in marznodes:
-        asyncio.create_task(node_service.get_nodes_logs(paneltype, marznode))
+    asyncio.create_task(start_marznode_tasks())
 
     yield
 
---
+++ app/tasks/marznode.py
@@ -0,0 +1,30 @@
+import asyncio
+from app.config import PANEL_ADDRESS, PANEL_CUSTOM_NODES, PANEL_PASSWORD, PANEL_USERNAME
+from app.models.panel import Panel
+from app.service.check_service import CheckService
+from app.service.marznode_service import TASKS, MarzNodeService
+from app.utils.panel import get_marznodes
+from app import user_limit_db, storage
+
+async def start_marznode_tasks():
+    paneltype = Panel(
+        username=PANEL_USERNAME,
+        password=PANEL_PASSWORD,
+        domain=PANEL_ADDRESS,
+    )
+
+    node_service = MarzNodeService(CheckService(
+        storage, user_limit_db))
+
+    marznodes = await get_marznodes(paneltype)
+
+    if PANEL_CUSTOM_NODES:
+        marznodes = [m for m in marznodes if m.name in PANEL_CUSTOM_NODES]
+
+    async with asyncio.TaskGroup() as tg:
+        for marznode in marznodes:
+            await node_service.create_node_task(paneltype, tg, marznode)
+            tg.create_task(
+                node_service.handle_cancel_all(TASKS, paneltype),
+                name="cancel_all",
+            )
