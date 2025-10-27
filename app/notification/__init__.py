538cde635f9f61e05ab1cbcd62cf546a458fcbd6
ben-aslan <kiziltahaaslan@gmail.com>
2025-10-27 23:55:28 +0330

    refactor(notifications): get ad from github api and change reload location

--- app/notification/__init__.py
+++ app/notification/__init__.py
@@ -1,13 +1,22 @@
+import base64
 import requests
 
-AD = requests.get(
-    "https://raw.githubusercontent.com/kizil-aslan/nobetci-ads/refs/heads/main/main.txt").text
+AD = base64.b64decode(requests.get(
+        f"https://api.github.com/repos/kizil-aslan/nobetci-ads/contents/main.txt", headers={
+            "Cache-Control": "no-cache, no-store, must-revalidate",
+            "Pragma": "no-cache",
+            "Expires": "0"
+        }).json()["content"]).decode("utf-8")
 
 
 def reload_ad():
     global AD
-    AD = requests.get(
-        "https://raw.githubusercontent.com/kizil-aslan/nobetci-ads/refs/heads/main/main.txt").text
+    AD = base64.b64decode(requests.get(
+        f"https://api.github.com/repos/kizil-aslan/nobetci-ads/contents/main.txt", headers={
+            "Cache-Control": "no-cache, no-store, must-revalidate",
+            "Pragma": "no-cache",
+            "Expires": "0"
+        }).json()["content"]).decode("utf-8")
 
 
 def get_ad():
